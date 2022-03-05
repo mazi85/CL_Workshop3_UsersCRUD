60450a59278e4d40fcff0d63442eb3efd81e85c1
Adrian Mazur <muzy1125@gmail.com>
2022-03-05 10:17:36 +0100

    Project WEB template - after instructions,new better order + all user functionality

--- src/main/java/pl/coderslab/mazi85/controllers/UserList.java
+++ src/main/java/pl/coderslab/mazi85/controllers/UserList.java
@@ -1,4 +1,7 @@
-package pl.coderslab.mazi85.users;
+package pl.coderslab.mazi85.controllers;
+
+import pl.coderslab.mazi85.dao.UserDao;
+import pl.coderslab.mazi85.entity.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,10 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/user/list")
 public class UserList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        UserDao userDao = new UserDao();
+        try {
+            List<User> allUsers = userDao.findAll();
+            request.setAttribute("allUsers",allUsers);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
     }
